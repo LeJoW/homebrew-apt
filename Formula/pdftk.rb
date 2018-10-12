63a78ec6b1fbf727d34c6f0c3ba55ca51d4376d1
LeJoW <elle.git.wilke@gmail.com>
2018-10-12 12:34:12 +0200

    debugging

--- Formula/pdftk.rb
+++ Formula/pdftk.rb
@@ -7,7 +7,7 @@ end
 class Pdftk < Formula
     desc "PDFtk Server is a command-line tool for working with PDFs. It is commonly used for client-side scripting or server-side processing of PDFs."
     homepage "http://www.pdflabs.com/tools/pdftk-server"
-    url "http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.11-setup.pkg"
+    url "http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.11-setup.pkg",
         :using => PkgExtract
     sha256 "c33cf95151e477953cd57c1ea9c99ebdc29d75f4c9af0d5f947b385995750b0c"
   
