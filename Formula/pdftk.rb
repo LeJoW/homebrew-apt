9a2bcd3e37e4059decd2c98f9b64a0544f226de5
LeJoW <elle.git.wilke@gmail.com>
2018-10-12 11:16:38 +0200

    debugging

--- Formula/pdftk.rb
+++ Formula/pdftk.rb
@@ -1,7 +1,14 @@
+class PkgExtract < CurlDownloadStrategy
+    def stage
+        safe_system 'echo', @tarball_path
+    end
+end
+
 class Pdftk < Formula
     desc "PDFtk Server is a command-line tool for working with PDFs. It is commonly used for client-side scripting or server-side processing of PDFs."
     homepage "http://www.pdflabs.com/tools/pdftk-server"
     url "http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.11-setup.pkg"
+        :using => PkgExtract
     sha256 "c33cf95151e477953cd57c1ea9c99ebdc29d75f4c9af0d5f947b385995750b0c"
   
     depends_on :macos => :lion
@@ -22,7 +29,7 @@ class Pdftk < Formula
         #system "./bootstrap" if build.head?
         #system "./configure", *args
         #system "make", "install"
-        safe_system "/usr/bin/xar", "-f", @tarball_path
+        safe_system 'echo', @tarball_path
     end
   
     test do
