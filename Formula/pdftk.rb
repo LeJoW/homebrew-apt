0a5ae93513f4817a4f05a2c0008d72f969c19503
LeJoW <elle.git.wilke@gmail.com>
2018-10-12 11:03:48 +0200

    debugging

--- Formula/pdftk.rb
+++ Formula/pdftk.rb
@@ -1,29 +1,29 @@
-require 'formula'
-
-class PkgExtract < CurlDownloadStrategy
-  def stage
-    safe_system 'xar', '-f', @tarball_path
-    #safe_system '/usr/bin/xar', '-xf', @tarball_path
-    #chdir
-    #safe_system 'mv *.pkg/Payload Payload.gz'
-    #safe_system 'ls | grep -v Payload | xargs rm -r'
-  end
-end
-
 class Pdftk < Formula
-  homepage 'http://www.pdflabs.com/tools/pdftk-server'
-  url 'https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.11-setup.pkg',
-    :using => PkgExtract
-  sha256 'c33cf95151e477953cd57c1ea9c99ebdc29d75f4c9af0d5f947b385995750b0c'
-
-  depends_on :macos => :lion
-
-  def install
-    #safe_system "/bin/pax --insecure -rz -f Payload.gz -s ',./bin,#{bin},' -s ',./man,#{man},' -s ',./lib,#{lib},' -s ',./license_gpl_pdftk,#{prefix}/LICENSE,' -s ',./,#{prefix}/README/,'"
-    safe_system "which `pax`"
-  end
-
-  test do
-    system "echo installed"
-  end
+    desc "PDFtk Server is a command-line tool for working with PDFs. It is commonly used for client-side scripting or server-side processing of PDFs."
+    homepage "http://www.pdflabs.com/tools/pdftk-server"
+    url "http://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.11-setup.pkg"
+    sha256 "c33cf95151e477953cd57c1ea9c99ebdc29d75f4c9af0d5f947b385995750b0c"
+  
+    def install
+        #args = %W[
+        #    --prefix=#{prefix}
+        #    --sysconfdir=#{etc}
+        #    --with-ssl=openssl
+        #    --with-libssl-prefix=#{Formula["openssl"].opt_prefix}
+        #    --disable-debug
+        #]
+        #
+        #args << "--disable-pcre" if build.without? "pcre"
+        #args << "--with-metalink" if build.with? "libmetalink"
+        #args << "--with-gpgme-prefix=#{Formula["gpgme"].opt_prefix}" if build.with? "gpgme"
+    
+        #system "./bootstrap" if build.head?
+        #system "./configure", *args
+        #system "make", "install"
+        system "pwd"
+    end
+  
+    test do
+        system "echo aze"
+    end
 end
